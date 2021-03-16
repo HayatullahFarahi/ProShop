84249e006542b13994f2ad659faa1bc7b532085f
hayatullah farahi <hayatullah.farahi@gmail.com>
2021-03-16 10:57:16 +0430

    error handling added in backend

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import colors from 'colors';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import connectDB from './config/db.js';
 
 import productRoutes from './routes/productRoutes.js';
@@ -17,12 +18,16 @@ app.get('/', (req, res) => {
 
 app.use('/api/products', productRoutes);
 
-const PORT = process.env.PORT || 5000;
+app.use(notFound);
+
+app.use(errorHandler);
+
+const PORT = 5000;
 
 app.listen(
     PORT,
     console.log(
         `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow
-            .bold
+            .inverse.bold
     )
 );
