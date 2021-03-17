6adc9ca3bb243862dbfeed327abb6472c0c475ef
hayatullah farahi <hayatullah.farahi@gmail.com>
2021-03-17 11:35:38 +0430

    Loader & Massage Components added

--- backend/server.js
+++ backend/server.js
@@ -22,12 +22,12 @@ app.use(notFound);
 
 app.use(errorHandler);
 
-const PORT = 5000;
+const PORT = 4000;
 
 app.listen(
     PORT,
     console.log(
         `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow
-            .inverse.bold
+            .bold
     )
 );
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -6,18 +6,15 @@ import Rating from '../components/Rating';
 
 const ProductScreen = ({ match }) => {
     const [product, setProduct] = useState([]);
-
     useEffect(() => {
         const fetchProduct = async () => {
             const { data } = await axios.get(
                 `/api/products/${match.params.id}`
             );
-
             setProduct(data);
         };
         fetchProduct();
     }, [match.params.id]);
-
     return (
         <>
             <Link className='btn btn-light my-3' to='/'>
