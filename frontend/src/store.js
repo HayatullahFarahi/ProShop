c1316288c8f20348bb8432ee95a97b3f0de07b1e
hayatullah farahi <hayatullah.farahi@gmail.com>
2021-03-23 19:05:47 +0430

    shipping screen added

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -31,12 +31,15 @@ const userInfoFromStorage = localStorage.getItem('userInfo')
     ? JSON.parse(localStorage.getItem('userInfo'))
     : null;
 
-console.log('TeSTING-----------------');
-console.log(cartItemsFromStorage);
-console.log(userInfoFromStorage);
+const shippingAddressFromStorage = localStorage.getItem('shippingAddress')
+    ? JSON.parse(localStorage.getItem('shippingAddress'))
+    : {};
 
 const initialState = {
-    cart: { cartItems: cartItemsFromStorage },
+    cart: {
+        cartItems: cartItemsFromStorage,
+        shippingAddress: shippingAddressFromStorage,
+    },
     userLogin: { userInfo: userInfoFromStorage },
 };
 
